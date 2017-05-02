c6cdaae1fceade21e09bdf043a7e97367a3f5686
Francisco Costa <a70922@uminho.pt>
2017-05-02 22:52:37 +0100

    Added some class's and project.bluej
    Start of the assignment

---
+++ src/UMer.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class UMer here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class UMer
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    
+    private ArrayList <Object> a;
+
+    /**
+     * Constructor for objects of class UMer
+     */
+    public UMer()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
