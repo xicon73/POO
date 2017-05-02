1391f7a7c240a8d53f5882a5fc62b7d702295843
Francisco Costa <a70922@uminho.pt>
2017-05-02 22:53:57 +0100

    Class's added
    Driver and Car class’s added

---
+++ src/Motorista.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Motorista here.
+ *
+ * @author my name
+ */
+
+public class Motorista
+{
+    // instance variables - replace the example below with your own
+    private String nome;
+    private 
+
+    /**
+     * Constructor for objects of class Motorista
+     */
+    public Motorista()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
