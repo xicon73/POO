40ffcca5d0b1b917745136fdd18c57154cba9422
Francisco Costa <xicon73@gmail.com>
2017-05-05 09:50:17 +0100

    changed driver's class

--- src/Motorista.java
+++ src/Motorista.java
@@ -7,143 +7,74 @@ import static java.lang.System.out;
  * @author my name
  */
 
-public class Motorista extends Cliente
-{
-    private String email;
-    private String password;
-    private String nome;
-    private String morada;
-    private GregorianCalendar data_nascimento;
+public class Motorista extends Cliente {
     private int[] grau = new int[100];    //grau de cumprimento de horário estabelecido com o cliente, dado por um factor entre 0 e 100
     private double kms;
     private int estado;
-    
+
     /**
      * Construtor vazio, ou seja, inicializa um Motorista a nulo.
      */
     public Motorista(){
-        this.email = "";
-        this.password = "";
-        this.nome = "";
-        this.morada = "";
-        this.data_nascimento = new GregorianCalendar();
+        super();
         this.grau = null;
         this.kms = 0;
         this.estado = 0;
     }
-    
+
     /**
      * Construtor parametrizado, ou seja, recebe um email, uma password, um nome, uma morada, uma data de nascimento e um grau.
-     * @param email Email do Motorista
-     * @param password Password do Motorista
-     * @param nome Nome do Motorista
-     * @param morada Morada do Motorista
-     * @param d Data de nascimento do Motorista
      * @param grau Grau de cumprimento de horário estabelecido com o Cliente
      * @param kms Número de kms já realizados na UMeR
+     * @param estado Estado do motorista, 0 - Fora de serviço, 1 - Livre, 2 - Em viagem
      */
-    public Motorista(String email, String password, String nome, String morada, GregorianCalendar d, int [] array, double kms){
-        this.email = email;
-        this.password = password;
-        this.nome = nome;
-        this.morada = morada;
-        this.data_nascimento = d;
+    public Motorista(String email, String password, String nome, String morada, GregorianCalendar d, int [] array, double kms, int estado){
+        super(email,password,nome,morada,d);
         this.grau = array;
         this.kms = kms;
+        this.estado = estado;
     }
-    
+
     /**
      * Construtor de cópia, ou seja, copia os dados de um Motorista já existente.
      * @param m O Motorista que vamos copiar.
      */
     public Motorista(Motorista m){
-        this.email = m.getEmail();
-        this.password = m.getPassword();
-        this.nome = m.getNome();
-        this.morada = m.getMorada();
-        this.data_nascimento = m.getDate();
+        super(m);
         this.grau = m.getGrau();
         this.kms = m.getKms();
+        this.estado = m.getEstado();
     }
-    
+
     /*
      * GETS
      */
-    /**
-     * A função getEmail devolve o email do Motorista.
-     */
-    public String getEmail(){ return email; }
-    
-    /**
-     * A função getPassword devolve a password do Motorista.
-     */
-    public String getPassword() { return password; }
-    
-    /**
-     * A função getNome devolve o nome do Motorista.
-     */
-    public String getNome(){ return nome; }
-    
-    /**
-     * A função getMorada devolve a morada do Motorista.
-     */
-    public String getMorada(){ return morada; }
-    
-    /**
-     * A função getDate devolve a Data de Nascimento do Motorista.
-     */
-    public GregorianCalendar getDate(){ return (GregorianCalendar)data_nascimento.clone(); }
-    
+
     /**
      * A função getGrau devolve o grau de cumprimento de horário estabelecido com o Cliente.
      */
     public int [] getGrau() { return grau.clone(); }
-    
+
     /**
      * A função getKms devolve o número de kms já realizados na UMeR.
      */
     public double getKms() { return kms; }
+
+    public int getEstado() {return estado;}
     /*
      * SETS
      */
     /**
-     * A função setEmail modifica o email do Motorista.
-     * @param email O novo email que irá substituir o antigo.
-     */
-    public void setEmail(String email){ this.email = email; }
-    
-    /**
-     * A função setPassword modifica a password do Motorista.
-     * @param pw A password que irá substituir a antiga.
-     */
-    public void setPassword(String pw) { password = pw; }
-    
-    /**
-     * A função setNome modifica o nome do Motorista.
-     * @param nome O novo nome que irá substituir o antigo.
-     */
-     public void setNome(String nome){ this.nome=nome; }
-     
-    /**
-     * A função setMorada modifica a morada do Motorista.
-     * @param morada A nova morada que irá substituir a antiga.
-     */
-    public void setMorada(String morada){ this.morada=morada; }
-    
-    /**
-     * A função setDate modifica a data de nascimento do Motorista.
-     * @param d A nova data de nascimento que irá substituir a antiga.
-     */
-    public void setDate(GregorianCalendar d){this.data_nascimento=(GregorianCalendar)d.clone();}
-    
-    /**
      * A função setGrau modifica o grau de cumprimento de horário estabelecido com o Cliente.
      */
     public void serGrau (int [] grau) { this.grau = grau.clone(); }
-    
+
     /**
      * A função setKms modifica o número de kms já realizados na UMeR.
      */
     public void setKms (double kms) { this.kms = kms; }
-}
 
+    public void setEstado(int estado) {
+      this.estado = estado;
+    }
+}
