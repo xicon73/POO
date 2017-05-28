6de2290e8c4df49926fe9c85a22e7349e1d34a4b
Francisco Costa <xicon73@gmail.com>
2017-05-28 11:44:07 +0100

    added state saving + somes new functions

---
+++ src/Persistência.java
@@ -0,0 +1,30 @@
+package src;
+import java.util.*;
+import java.io.*;
+
+
+public class Persistencia implements Serializable{
+    /**
+     * A função guardarEstado permite guardar um estado em binario.
+     * @param e O Core recebido.
+     */
+    public void guardarEstado(Core e) throws IOException{
+        FileOutputStream fos = new FileOutputStream("GeocachingPOO.state");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        oos.writeObject(e);
+        oos.close();
+    }
+
+    /**
+     * A função carregarEstado permite carregar um estado.
+     */
+    public Core carregarEstado() throws IOException,ClassNotFoundException{
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("GeocachingPOO.state"));
+
+        Core novoState = (Core) ois.readObject();
+        ois.close();
+
+        return novoState;
+    }
+}
